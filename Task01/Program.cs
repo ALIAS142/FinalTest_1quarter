dfcb53ab42b0b46f28d87707bc7127e210aa9451
Anna <kochurovaav@gmail.com>
2023-03-11 17:23:27 +0200

    First method in Task01 is done

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,4 +1,19 @@
 ﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
 //  Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
-//  
+//
+
+string[] array1 = new string[7] { "Anna", "Elena", "Ola", "Tom", "Alex", "Kit", "Sveta" };
+string[] array2 = new string[array1.Length];
+void Find3DigitsArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
 
